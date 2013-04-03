4c4890b12b88dfa5bfe662d94721ef884f39184c
Hela MAHFOUDHI <ridha_hela@yahoo.fr>
2013-04-03 10:57:28 +0200

    updating blogSource

---
+++ InvenietisPortal/InvenietisBlog/BlogArticlePublished.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog
+{
+    public class BlogArticlePublished
+    {
+        readonly BlogArticle _article;
+        private DateTimeOffset _publicationDate;
+        private string _titleFR;
+        private string _titleEN;
+        private string _htmlAbstractFR;
+        private string _htmlAbstractEN;
+
+        internal BlogArticlePublished(BlogArticle a)
+        {
+            _article = a;
+        }
+
+        public BlogArticle Article { get { return _article; } }
+
+        public bool IsHidden { get { return Article.Status != BlogArticleStatus.Published; } }
+
+        public DateTimeOffset PublicationDate { get { return DateTime.Today; } set { PublicationDate = _publicationDate; } }
+
+        public string TitleFR { get { return null; } set { TitleFR = _titleFR; } }
+
+        public string TitleEN { get { return null; } set { TitleEN = _titleEN; } }
+
+        public string HtmlAbstractFR { get { return null; } set { HtmlAbstractFR = _htmlAbstractFR; } }
+
+        public string HtmlAbstractEN { get { return null; } set { HtmlAbstractEN = _htmlAbstractEN; } }
+
+    }
+}
---
+++ InvenietisPortal/InvenietisBlog/BlogLanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog
+{
+    public enum BlogLanguage
+    {
+        None = 0,
+        English = 1,
+        French = 2
+    }
+}
