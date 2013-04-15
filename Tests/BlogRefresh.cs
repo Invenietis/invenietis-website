2961da31339ab3ed07dc8b6f7bba1a48e39c6686
Hela MAHFOUDHI <ridha_hela@yahoo.fr>
2013-04-15 15:02:38 +0200

    display blog's contents on invenietis website

--- Tests/BlogRefresh.cs
+++ Tests/BlogRefresh.cs
@@ -22,6 +22,7 @@ namespace Tests
         {
             Source.CreateBlog( new Uri( "http://macdarwin.github.com/atom.xml" ) );
             Assert.That( Source.AuthorName == "Guillaume Fradet" , "name "+Source.AuthorName);
+           
         }
         [Test]
         public void LoadLastRefresh()
@@ -29,7 +30,8 @@ namespace Tests
             BlogRefreshResult = Source.RefreshFromUri( new Uri( "http://macdarwin.github.com/atom.xml" ) );
             Assert.That( BlogRefreshResult.IsSuccess );
             Assert.That( BlogRefreshResult.DisappearedArticleCount == 0, BlogRefreshResult.DisappearedArticleCount.ToString() );
-            Assert.That( BlogRefreshResult.NewArticleCount != 0, BlogRefreshResult.NewArticleCount.ToString() );  
+            Assert.That( BlogRefreshResult.NewArticleCount != 0, BlogRefreshResult.NewArticleCount.ToString() );
+            Assert.That( Source.Articles.Count == 3);
         }
 
         [Test]
