ed4966abbd01a4b85d1bfcd39aa3aac345e8ac1f
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2016-05-04 18:04:11 +0200

    Back setup

--- Invenietis.Back/Startup.cs
+++ Invenietis.Back/Startup.cs
@@ -1,27 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using Invenietis.Common.Cultures;
 using Invenietis.Data;
-using Invenietis.LocalizedRoutes;
-using Invenietis.LocalizedRoutes.Config;
-using Invenietis.LocalizedRoutes.Mvc;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Localization;
-using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.Razor;
-using Microsoft.AspNet.Routing;
-using Microsoft.AspNet.Routing.Template;
+using Microsoft.AspNet.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Q = Invenietis.Repositories.Queries;
+using C = Invenietis.Repositories.Commands;
 
-namespace Invenietis.Web
+namespace Invenietis.Back
 {
     public class Startup
     {
@@ -31,7 +23,6 @@ namespace Invenietis.Web
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddJsonFile("routes.json")
                 .AddJsonFile("config.json")
                 .AddJsonFile("cultures.json");
 
@@ -47,13 +38,6 @@ namespace Invenietis.Web
             // Add framework services.
             services.AddMvc();
 
-            // Localized routes provider
-            var routeProvider = new LocalizedRouteProvider( Configuration.Get<LocalizedRouteConfig>() );
-            var mvcAdapter = new LocalizedRoutesMvcAdapter( routeProvider, typeof(Startup).GetTypeInfo().Assembly );
-
-            services.AddInstance( routeProvider );
-            services.AddInstance( mvcAdapter );
-
             // Cultures provider
             var cultureProvider = Configuration.Get<CultureProvider>();
             services.AddInstance( cultureProvider );
@@ -62,10 +46,14 @@ namespace Invenietis.Web
             services.AddSingleton<Q.ProjectRepository>();
             services.AddSingleton<Q.LearningRepository>();
             services.AddSingleton<Q.ClientRepository>();
+
+            services.AddSingleton<C.ProjectRepository>();
+            services.AddSingleton<C.LearningRepository>();
+            services.AddSingleton<C.ClientRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure( IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, LocalizedRouteProvider routeProvider, LocalizedRoutesMvcAdapter routeMvcAdapter )
+        public void Configure( IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory )
         {
             loggerFactory.AddConsole( Configuration.GetSection( "Logging" ) );
             loggerFactory.AddDebug();
@@ -84,41 +72,16 @@ namespace Invenietis.Web
             if( String.IsNullOrEmpty( dbPath ) ) throw new ArgumentNullException( "DatabasePath must be specified in config.json" );
             DataContext.GetDefault = () => new DataContext( dbPath );
 
-            // Localization
-            var cultureConfig = Configuration.Get<CultureConfig>();
-            ConfigureLocalization( app, cultureConfig );
-
-            // Build localized routes
-            routeProvider.SetupCultures( cultureConfig );
-            routeProvider.Build();
-
+            // Build routes
             app.UseMvc( routes =>
             {
-                // Map the localized routes for MVC controllers/actions
-                routeMvcAdapter.MapLocalizedRoutes( routes );
-
-                // Default route to choose the right localization
-                routes.MapRoute( "Default", "", new { controller = "Localization", action = "Index" } );
+                routes.MapRoute( name: "default",
+                    template: "{controller}/{action}",
+                    defaults: new { controller = "Home", action = "Index" } );
             } );
         }
 
-        // Change culture if url starts with '/fr' or '/en', ...
-        private void ConfigureLocalization( IApplicationBuilder app, ICultureConfig cultureConfig )
-        {
-            RequestCulture defaultCulture = new RequestCulture( cultureConfig.DefaultCulture );
-
-            RequestLocalizationOptions localizationOptions = new RequestLocalizationOptions();
-            localizationOptions.SupportedCultures = cultureConfig.SupportedCultures.Select( x => new CultureInfo( x ) ).ToList();
-
-            localizationOptions.RequestCultureProviders = new List<IRequestCultureProvider>()
-            {
-                 new UrlCultureProvider( cultureConfig )
-            };
-
-            app.UseRequestLocalization( localizationOptions, defaultCulture );
-        }
-
         // Entry point for the application.
-        public static void Main( string[] args ) => WebApplication.Run<Startup>( args );
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
