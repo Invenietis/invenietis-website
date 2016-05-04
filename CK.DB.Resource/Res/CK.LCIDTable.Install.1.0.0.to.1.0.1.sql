4c2eb0c79ff8415429c5a988f60f9b024022edcc
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2016-05-04 14:09:53 +0200

    Remove database

--- CK.DB.Resource/Res/CK.LCIDTable.Install.1.0.0.to.1.0.1.sql
+++
@@ -1,6 +0,0 @@
-﻿--[beginscript]
-
-UPDATE CK.tLCID 
-SET NativeName = UPPER(LEFT(NativeName, 1)) + LOWER(SUBSTRING(NativeName, 2, LEN(NativeName)));
-
---[endscript]
--- dbsetup.bat
+++
@@ -1,2 +0,0 @@
-Tools\DBSetup\CK.Deploy.Console.exe -v2 %CD%   "" "Invenietis.Web" "CK.DB.Resource;Invenietis.Database" "Server=.;Database=Invenietis;Trusted_Connection=True;Asynchronous Processing=True" 
-pause
